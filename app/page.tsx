02e64d716d39b3806e034a00bbf06f82354bd589
hodifa <databaserepos@gmail.com>
2024-09-27 06:01:58 +0200

    remove ts force rules

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { PiPlayCircle } from "react-icons/pi";
-import { motion } from "framer-motion";
 import ScrollAnimation from "./components/scroll-animation";
 
 export default function Home() {
